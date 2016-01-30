fa53e675ccff0c59c9bd753c0df1d3ce8f988747
koffeekingkong <koffeekingkong@gmail.com>
2016-01-30 16:08:00 +0800

    Week 5 in class exercises

--- week5_working_files/assignment/starter_code/index.js
+++ week5_working_files/assignment/starter_code/index.js
@@ -4,26 +4,26 @@ function changeCity() {
   var city = $("#city-type").val();
   $("#city-type").val(''); //sets the input field to empty
   city = city.toLowerCase().trim();
-  // trim will remove white space
+  //trim will remove white space
 
-  if (city === "NYC" || city === "nyc" || city === "New York City") {
+  if (city === "NYC" || city === "nyc" || city === "new york city") {
     $("body").attr("class", "nyc");
     //attr cleans out the class and replacing it with just the one you want - in this case its nyc//
     // console.log(city);
 
-  } else if (city === "sf" || city === "Bay Area") {
+  } else if (city === "sf" || city === "bay area" || city === "san francisco") {
     $("body").attr("class", "sf");
 
 
-  } else if (city === "la" || city === "Los Angeles") {
+  } else if (city === "la" || city === "los angeles") {
     $("body").attr("class", "la");
 
 
-  } else if (city === "austin" || city === "ATX") {
+  } else if (city === "austin" || city === "atx") {
     $("body").attr("class", "austin");
 
 
-  } else if (city === "sydney" || city === "SYD") {
+  } else if (city === "sydney" || city === "syd") {
     $("body").attr("class", "sydney");
 
   } else {
